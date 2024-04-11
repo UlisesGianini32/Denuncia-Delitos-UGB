888759c690b0716a854cddb90ac54cbcd52e22c5
New Llano 4k <146961271+UlisesGianini32@users.noreply.github.com>
2024-04-11 01:27:15 -0700

    push 11

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,34 +1,61 @@
 <?php
+
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use GuzzleHttp\Psr7\Message;
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
+    //
+
+
+    public function login(Request $request) {
         $loginData = $request->validate([
-            'email' => 'email|required',
+            'email' => 'required',
             'password' => 'required'
         ]);
-    
-        if (!auth()->attempt($loginData)) {
-    
-            return response([
-                'response' => 'Invalid Credentials',
-                'message' => 'error'
-            ]);
-        }
-    
-        $accessToken = auth()->user()->createToken('authToken') -> accessToken;
-    
-        return response([
-            'profile' =>auth()->user(),
-            'accessToken' => $accessToken,
-            'message' => 'success'
-        ]);
-    } 
-}
 
+        if(!auth()->attempt($loginData))
+        {
+            return response()->json([
+                'response' => 'Contraseña o correo incorrecto',
+                'success' => false
+            ], 401);
+        }
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response()->json([
+            'profile' => auth()->user(),
+            'access_token' => $accessToken,
+            'success' => true
+        ]);
+    }
+
+
+    public function logout(Request $request) {
+
+        $request->user()->token()->revoke();
+
+        return response()->json([
+
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function register(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+        $user = User::create($validatedData);
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([ 'profile' => $user, 'access_token' => $accessToken]);
+    }
+
+}
