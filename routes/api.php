74d0e56163c91396f242e85e5ea5d08d91abce28
New Llano 4k <146961271+UlisesGianini32@users.noreply.github.com>
2024-08-22 05:41:33 -0700

    cfeapp28

--- routes/api.php
+++ routes/api.php
@@ -31,25 +31,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/lot', [LotController::class, 'list']);
 Route::get('/lot/{id}', [LotController::class, 'item']);
 Route::post('/lot/create', [LotController::class, 'create']);
-Route::post('/lot/update{id}', [LotController::class, 'update']);
+Route::post('/lot/update/{id}', [LotController::class, 'update']);  // CORREGIDO
 Route::post('/lot/delete/{id}', [LotController::class, 'delete']);
 
 Route::get('/product', [ProductController::class, 'list']);
 Route::get('/product/{id}', [ProductController::class, 'item']);
 Route::post('/product/create', [ProductController::class, 'create']);
-Route::post('/product/update{id}', [ProductController::class, 'update']);
+Route::post('/product/update/{id}', [ProductController::class, 'update']);  // CORREGIDO
 Route::post('/product/delete/{id}', [ProductController::class, 'delete']);
 
 Route::get('/employee', [EmployeeController::class, 'list']);
 Route::get('/employee/{id}', [EmployeeController::class, 'item']);
 Route::post('/employee/create', [EmployeeController::class, 'create']);
-Route::post('/employee/update{product_id}', [EmployeeController::class, 'update']);
+Route::post('/employee/update/{id}', [EmployeeController::class, 'update']);  // CORREGIDO
 Route::post('/employee/delete/{id}', [EmployeeController::class, 'delete']);
 
 Route::get('/entry', [EntryController::class, 'list']);
 Route::get('/entry/{id}', [EntryController::class, 'item']);
 Route::post('/entry/create', [EntryController::class, 'create']);
-Route::post('/entry/update{id}', [EntryController::class, 'update']);
+Route::post('/entry/update/{id}', [EntryController::class, 'update']);  // CORREGIDO
 Route::post('/entry/delete/{id}', [EntryController::class, 'delete']);
 
 Route::post('/auth/login', [AuthController::class, 'login']);
